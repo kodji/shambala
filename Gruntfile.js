4f3e451c35b340e6617264b4d8346d80154d1c6a
kodji <k@odj.in>
2014-03-09 18:23:06 +0400

    some updates

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jade: {
+      prod:{
+        options: {
+          pretty: false
+        },
+        files: {
+          '../dist/index.html': ['index.jade']
+        }        
+      }
+    },
+    less: {
+      prod: {
+        options: {
+          paths: ["less"],
+          cleancss: true,
+          compress: true,
+          ieCompat: true,
+          modifyVars: {
+          }
+        },
+        files: {
+          "../dist/css/style.css": "less/style.less"
+        }
+      }
+    },
+    watch: {
+      files: ['less/**/*','js/**/*','*.jade','Gruntfile.js'],
+      tasks: ['jade:prod','less:prod'] 
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.registerTask('default', ['jade:prod','less:prod']);
+};
---
+++ config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+require.config({
+  paths: {
+    jquery:'./bower_components/jquery/dist',
+    app: '/app',
+    components: '../components',
+    knockout: './bower_components/knockoutjs/output/knockout-latest.js'
+  }
+});
